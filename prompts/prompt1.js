eb31d964e99156995f1beacfb6c07acf107a497c
unknown <ryanvolum@gmail.com>
2022-01-27 18:27:36 -0800

    fixed minor prompt bugs

--- prompts/prompt1.js
+++ prompts/prompt1.js
@@ -1,6 +1,6 @@
 let basePrompt = `/* This document contains a BabylonJS scene, natural language commands and the BabylonJS code needed to accomplish them */
 
-let state = {};
+state = {};
 
 /* Make the light more intense */
 BABYLON.Engine.LastCreatedScene.lights[0].intensity = 10
@@ -17,22 +17,6 @@ state.cube.position.y += 1
 /* Move it to the left */
 state.cube.position.x -= 1
 
-/* Make it change color when the mouse is over it */
-state.cube.actionManager = new BABYLON.ActionManager(BABYLON.Engine.LastCreatedScene);
-
-state.hoverAction = new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function () {
-state.cube.material = new BABYLON.StandardMaterial("mat", BABYLON.Engine.LastCreatedScene);
-state.cube.material.diffuseColor = new BABYLON.Color3(1, 0, 0);
-});
-
-unHoverAction = new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOutTrigger, function () {
-state.cube.material = new BABYLON.StandardMaterial("mat", BABYLON.Engine.LastCreatedScene);
-state.cube.material.diffuseColor = new BABYLON.Color3(0, 1, 1);
-});
-
-state.cube.actionManager.registerAction(hoverAction);
-state.cube.actionManager.registerAction(unHoverAction);
-
 /* Make the block teal */
 state.cube.material = new BABYLON.StandardMaterial("mat", BABYLON.Engine.LastCreatedScene);
 state.cube.material.diffuseColor = new BABYLON.Color3(0, 1, 1);
@@ -44,6 +28,22 @@ state.spinningCube = setInterval(() => {
 
 /* Make it stop */
 clearInterval(state.spinningCube)
+
+/* Make it change color when the mouse is over it */
+state.cube.actionManager = new BABYLON.ActionManager(BABYLON.Engine.LastCreatedScene);
+
+state.hoverAction = new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function () {
+state.cube.material = new BABYLON.StandardMaterial("mat", BABYLON.Engine.LastCreatedScene);
+state.cube.material.diffuseColor = new BABYLON.Color3(1, 0, 0);
+});
+
+state.unHoverAction = new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOutTrigger, function () {
+state.cube.material = new BABYLON.StandardMaterial("mat", BABYLON.Engine.LastCreatedScene);
+state.cube.material.diffuseColor = new BABYLON.Color3(0, 1, 1);
+});
+
+state.cube.actionManager.registerAction(state.hoverAction);
+state.cube.actionManager.registerAction(state.unHoverAction);
 
 /* Put a sphere on top of the cube */
 state.sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 1}, BABYLON.Engine.LastCreatedScene)
@@ -59,8 +59,9 @@ for (let i = 0; i < 10; i++) {
     state.torusArray[i].position.y = i;
 }
 
-/* Delete the sphere */
-BABYLON.Engine.LastCreatedScene.meshes[1].dispose()
+/* Delete the sphere and the cube */
+state.sphere.dispose()
+state.cube.dispose()
 
 /* Delete the torusses */
 for (let i = 0; i < torusArray.length; i++) {
