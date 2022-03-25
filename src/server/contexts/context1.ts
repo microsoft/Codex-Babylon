866f4cc523e724af3be16d38d52bcd3e0d072c90
Vivien Hung <windfish_hung@hotmail.com>
2022-03-25 13:45:46 -0700

    Convert backend code to TypeScript

---
+++ src/server/contexts/context1.ts
@@ -0,0 +1,66 @@
+export const baseContext = `/* This document contains a BabylonJS scene, natural language commands and the BabylonJS code needed to accomplish them */
+
+state = {};
+
+/* Make a cube */
+state.cube = BABYLON.MeshBuilder.CreateBox("cube", {size: 1}, scene);
+
+/* Move the cube up */
+state.cube.position.y += 1
+
+/* Move it to the left */
+state.cube.position.x -= 1
+
+/* Make the block teal */
+state.cube.material = new BABYLON.StandardMaterial("mat", scene);
+state.cube.material.diffuseColor = new BABYLON.Color3(0, 1, 1);
+
+/* Now make it spin */
+state.spinningCube = setInterval(() => {
+    scene.meshes[0].rotation.y += 0.02
+}, 10)
+
+/* Make it stop */
+clearInterval(state.spinningCube)
+
+/* Make it change color when the mouse is over it */
+state.cube.actionManager = new BABYLON.ActionManager(scene);
+
+state.hoverAction = new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function () {
+state.cube.material = new BABYLON.StandardMaterial("mat", scene);
+state.cube.material.diffuseColor = new BABYLON.Color3(1, 0, 0);
+});
+
+state.unHoverAction = new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOutTrigger, function () {
+state.cube.material = new BABYLON.StandardMaterial("mat", scene);
+state.cube.material.diffuseColor = new BABYLON.Color3(0, 1, 1);
+});
+
+state.cube.actionManager.registerAction(state.hoverAction);
+state.cube.actionManager.registerAction(state.unHoverAction);
+
+/* Put a sphere on top of the cube */
+state.sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 1}, scene)
+state.sphere.position.y = 1
+
+/* Delete the sphere and the cube */
+state.sphere.dispose()
+state.cube.dispose()
+
+/* make 50 cubes side by side */
+state.cubes = [];
+for (let i = 0; i < 50; i++) {
+    state.cubes[i] = BABYLON.MeshBuilder.CreateBox("cube", {size: 1}, scene);
+    state.cubes[i].position.x = i;
+}
+
+/* stack them like stairs */
+for (let i = 0; i < 50; i++) {
+    state.cubes[i].position.y = i;
+}
+
+/* remove them */
+for (let i = 0; i < 50; i++) {
+    state.cubes[i].dispose();
+}
+`;
---
+++ src/server/model.ts
@@ -0,0 +1,49 @@
+require("dotenv").config();
+import fetch from "isomorphic-fetch";
+
+// Contains the helper methods for interacting with Codex and crafting model prompts
+import { baseContext } from "./contexts/context1";
+import Context from "./Context";
+
+const maxPromptLength = 3200;
+
+// CURRENTLY SINGLE TENANT - WOULD NEED TO UPDATE THIS TO A MAP OF TENANT IDs TO PROMPTS TO MAKE MULTI-TENANT
+export const context = new Context(baseContext);
+
+export async function getCompletion(command) {
+    let prompt = context.getPrompt(command);
+
+    if (prompt.length > maxPromptLength) {
+        context.trimContext(maxPromptLength - command.length + 6); // The max length of the prompt, including the command, comment operators and spacing.
+    }
+
+    const response = await fetch(`${process.env.ENDPOINT}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.CODEX_API_KEY}`
+        },
+        body: JSON.stringify({
+            prompt,
+            max_tokens: 800,
+            temperature: 0,
+            stop: "/*",
+            n: 1
+            // model: `${process.env.MODEL}`
+        })
+    });
+
+    // catch errors
+    if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+    }
+
+    const json = await response.json();
+    let code = json.choices[0].text;
+    context.addInteraction(command, code);
+
+    return {
+        code,
+        prompt
+    };
+}
